f15e7a6bf20af70b7fd8cc875a82e52690efabc9
Xavier-Hsiao <tim88776@gmail.com>
2023-11-25 14:49:31 +0800

    Build: Complete basic styling of index page

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const { engine } = require("express-handlebars");
 const port = 3000;
 
-app.engine("handlebars", engine({extname: ".hbs"}));
+app.engine(".hbs", engine({extname: ".hbs"}));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("This is my first route!");
+  res.render("index");
 });
 
 app.listen(port, () => {
