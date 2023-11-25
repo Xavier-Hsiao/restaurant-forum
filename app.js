4f73f868e71bc3abe2a5f0b0abfcd75861c920c8
Xavier-Hsiao <tim88776@gmail.com>
2023-11-25 15:09:15 +0800

    Build: render restaurant list from json file to index page

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const app = express();
 const { engine } = require("express-handlebars");
 const port = 3000;
 
-app.engine(".hbs", engine({extname: ".hbs"}));
+// 導入 json 靜態資料，變數值為陣列
+const restaurants = require("./public/json/restaurant.json").results;
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { restaurants });
 });
 
 app.listen(port, () => {
